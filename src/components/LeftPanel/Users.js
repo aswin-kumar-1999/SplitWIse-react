4b8629b55df6f42feedb3699a3c1573bef1f87ed
Aswin Kumar <aswin.kumar.17@mountblue.tech>
2021-10-14 17:46:07 +0530

    Style NavLink

--- src/components/LeftPanel/Users.js
+++ src/components/LeftPanel/Users.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { Link } from "react-router-dom";
+import {  NavLink } from "react-router-dom";
 
 export default class Users extends Component {
     constructor(props) {
@@ -12,7 +12,7 @@ export default class Users extends Component {
 
     render() {
         return (
-            <Link to={this.props.uname}>
+            <NavLink activeClassName="active" className="navLink" to={this.props.uname}>
                 <div
                     style={{ display: this.props.display }}
                     className="users-groups"
@@ -22,7 +22,7 @@ export default class Users extends Component {
                     <i className="fa fa-user" style={{ padding: "5px" }}></i>
                     {this.props.uname}
                 </div>
-            </Link>
+            </NavLink>
         );
     }
 }
