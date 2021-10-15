afa1a652ca10ea82c08e52abcd9a100e807e8a4d
Aswin Kumar <aswin.kumar.17@mountblue.tech>
2021-10-15 07:38:26 +0530

    Storing dynamic data in Store.js

--- src/components/Store/Store.js
+++ src/components/Store/Store.js
@@ -1,3 +1,41 @@
-export const dataStore=()=>{
+export const transaction = require('../../data/transaction.json');
+export const group = require('../../data/group.json');
+export const user = require('../../data/user.json');
+
+
+
+export const dataStore = (amount, paid_by, owes, desc, groupName) => {
+    const lastTransaction = +transaction.last + 1;
+    transaction[lastTransaction] = {
+        amount,
+        paid_by,
+        owes,
+        desc
+    }
+    if (groupName === 'No group') {
+        for (let name of owes) {
+
+            user[paid_by] = {
+                [name]: user[paid_by].name ?? [],
+                owes: user[paid_by].owes ?? []
+            }
+            user[paid_by] = {
+                [name]: [...user[paid_by][name], lastTransaction]
+            }
+
+            user[name] = user[name] ?? {};
+            user[name] = {
+                owes: user[name].owes ?? []
+            }
+            user[name] = {
+                owes: [...user[name].owes, paid_by]
+            }
+        }
+    }
+    else {
+        group[groupName] = {
+            transaction: [...group[groupName].transaction, lastTransaction]
+        }
+    }
 
 }
