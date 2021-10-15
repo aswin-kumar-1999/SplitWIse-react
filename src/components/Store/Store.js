108bf568f6643903ec692401333175d54936574e
Aswin Kumar <aswin.kumar.17@mountblue.tech>
2021-10-15 23:31:27 +0530

    Solved bug in dataStore

--- src/components/Store/Store.js
+++ src/components/Store/Store.js
@@ -16,7 +16,7 @@ export const dataStore = (amount, paid_by, owes, desc, groupName) => {
         updateGroup = group;
         updateUser = user;
     }
-    
+    // console.log(updateUser)
     lastTransaction += 1;
     updateTransaction[lastTransaction] = {
         amount,
@@ -24,26 +24,30 @@ export const dataStore = (amount, paid_by, owes, desc, groupName) => {
         owes,
         desc
     }
-    updateTransaction.last=lastTransaction;
+    updateTransaction.last = lastTransaction;
     if (groupName === 'No group') {
-        for (let name of owes) {
-            updateUser[paid_by]= updateUser[paid_by] ?? {}
-            updateUser[paid_by] = {
-                [name]: updateUser[paid_by].name ?? [],
+        updateUser[paid_by] = updateUser[paid_by] ?? {}
+        console.log("owe",owes)
+        owes.forEach(name=>{
+            updateUser[paid_by] = { ...updateUser[paid_by],
+                [name]: updateUser[paid_by][name] ?? [],
                 owes: updateUser[paid_by].owes ?? []
             }
             updateUser[paid_by] = {
-                [name]: [...updateUser[paid_by][name], lastTransaction]
+                ...updateUser[paid_by],
+                [name]: [...updateUser[paid_by][name], lastTransaction],      
             }
 
             updateUser[name] = updateUser[name] ?? {};
             updateUser[name] = {
+                ...updateUser[name],
                 owes: updateUser[name].owes ?? []
             }
             updateUser[name] = {
+                ...updateUser[name],
                 owes: [...updateUser[name].owes, paid_by]
             }
-        }
+        })
     }
     else {
         updateGroup[groupName] = {
@@ -51,5 +55,50 @@ export const dataStore = (amount, paid_by, owes, desc, groupName) => {
             users: [...updateGroup[groupName].users]
         }
     }
+    console.log("Update Data", updateUser, updateTransaction, updateGroup)
+}
 
+export const settler = (userName) => {
+
+    if (updateTransaction === undefined) {
+        console.log("unpdate empty")
+        updateTransaction = transaction;
+        updateGroup = group;
+        updateUser = user;
+    }
+
+    //Lent
+    for (let oweTo in updateUser[userName]) {
+        for (let transactionId of updateUser[userName][oweTo]) {
+            delete updateTransaction[transactionId]
+        }
+    }
+    console.log(updateUser[userName])
+    //Owe
+    updateUser[userName].owes.forEach(name => {
+        updateUser[name][userName].forEach(transactionId => {
+            if (updateTransaction[transactionId].owes.length === 1) {
+                delete updateTransaction[transactionId]
+            } else {
+                updateTransaction[transactionId].owes = updateTransaction[transactionId].owes.filter(name => name !== userName)
+            }
+        })
+    });
+    delete updateUser[userName];
+    console.log("Deleted User", updateUser);
+    console.log("Transaction", updateTransaction)
+    // for (let group in updateGroup) {
+    //     if (updateGroup[group].users.includes(user)) {
+    //         for (let transactionId of updateGroup[group].transaction) {
+    //             if (updateTransaction[transactionId].paid_by === user) {
+    //                 delete updateTransaction[transactionId]
+    //             }
+    //             else if (updateTransaction[transactionId].owes.includes(user)) {
+    //                 updateTransaction[transactionId].owes =
+    //                     updateTransaction[transactionId].owes.
+    //                         filter((friends) => friends !== user)
+    //             }
+    //         }
+    //     }
+    // }
 }
